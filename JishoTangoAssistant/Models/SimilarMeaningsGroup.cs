c9a119798a7f31a0163ce853afdcac63d7c996e9
Minh Bang Vu <minhbangvu.sec@gmail.com>
2024-04-14 12:13:05 +0200

    Move non-UI logic of current user input selection from view to a service

---
+++ JishoTangoAssistant/Models/SimilarMeaningsGroup.cs
@@ -0,0 +1,3 @@
+namespace JishoTangoAssistant.Models;
+
+public record SimilarMeaningsGroup(TrulyObservableCollection<Meaning> SimilarMeanings);
--- JishoTangoAssistant/UI/ViewModel/JapaneseUserInputViewModel.cs
+++ JishoTangoAssistant/UI/ViewModel/JapaneseUserInputViewModel.cs
@@ -1,18 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
 using System.Linq;
-using Avalonia.Controls.ApplicationLifetimes;
+using System.Text;
 using Avalonia.Media;
-using JishoTangoAssistant.UI.Elements;
-using JishoTangoAssistant.UI.View;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using System.Threading.Tasks;
-using JishoTangoAssistant.Interfaces;
 using JishoTangoAssistant.Utils;
 using JishoTangoAssistant.Models;
-using JishoTangoAssistant.Models.Jisho;
 using JishoTangoAssistant.Services;
 
 namespace JishoTangoAssistant.UI.ViewModel;
@@ -33,22 +31,21 @@ public partial class JapaneseUserInputViewModel : JishoTangoAssistantViewModelBa
     private bool showFrontSide = true;
 
     [ObservableProperty]
-    private ObservableCollection<string> meanings = new ObservableCollection<string>();
+    private ObservableRangeCollection<SimilarMeaningsGroup> meanings = [];
 
-    [ObservableProperty]
-    private ObservableCollection<int> selectedIndicesOfMeanings = new ObservableCollection<int>();
+    private readonly List<int> selectedIndicesOfMeanings = [];
     
     [ObservableProperty]
     private string additionalComments = string.Empty;
 
     [ObservableProperty]
-    private ObservableCollection<string> words = new ObservableCollection<string>();
+    private ObservableCollection<string> words = [];
 
     [ObservableProperty]
     private int selectedIndexOfWords = -1;
 
     [ObservableProperty]
-    private ObservableCollection<string> otherForms = new ObservableCollection<string>();
+    private ObservableCollection<string> otherForms = [];
 
     [ObservableProperty]
     private int selectedIndexOfOtherForms = -1;
@@ -66,22 +63,26 @@ public partial class JapaneseUserInputViewModel : JishoTangoAssistantViewModelBa
     private Color textInputBackground = App.UsesDarkMode() ? Color.Parse("#66000000") : Color.Parse("#66ffffff");
 
     #endregion
-    public delegate void UpdateCheckBoxesEventHandler(int dataLength, IList<int> meaningsLengths, IList<string> flattenedMeanings);
-    public event UpdateCheckBoxesEventHandler? UpdateCheckBoxesEvent;
-
-    public delegate void ClearCheckBoxesEventHandler();
-    public event ClearCheckBoxesEventHandler? ClearCheckBoxesEvent;
-
-    private const string JishoTagUsuallyInKanaAlone = "Usually written using kana alone";
-
-    private readonly IJishoWebService jishoWebService;
+    
+    private readonly CurrentJapaneseUserInputSelectionService currentSelectionService;
 
     public JapaneseUserInputViewModel()
     {
-        SelectedIndicesOfMeanings.CollectionChanged += (_, _) => ChangeReadingOutput();
-        CurrentSession.VocabularyListService.GetList().CollectionChanged += (_, _) => UpdateTextInputBackground();
+        currentSelectionService = new CurrentJapaneseUserInputSelectionService(); // TODO DI
+        Words = currentSelectionService.GetWords();
+        OtherForms = currentSelectionService.GetOtherForms();
+        Meanings = currentSelectionService.GetMeanings();
 
-        jishoWebService = new JishoWebService(); // TODO DI
+        Meanings.CollectionChanged += MeaningsUpdateCollectionChanged;
+        Meanings.CollectionChanged += AutoEnableIfOnlyMeaning;
+
+        CurrentSession.VocabularyListService.GetList().CollectionChanged += (_, _) => UpdateTextInputBackground();
+    }
+
+    private void AutoEnableIfOnlyMeaning(object? sender, NotifyCollectionChangedEventArgs e)
+    {
+        if (e is not { NewItems.Count: 1, Action: NotifyCollectionChangedAction.Reset } && currentSelectionService.OnlyOneMeaningInSelection()) 
+            currentSelectionService.ChangeIsEnabledForAllMeanings(true);
     }
 
     #region auto-properties
@@ -90,54 +91,58 @@ public partial class JapaneseUserInputViewModel : JishoTangoAssistantViewModelBa
     {
         get
         {
-            var outputText = string.Empty;
+            var outputTextStringBuilder = new StringBuilder();
 
-            if (JapaneseToEnglishDirection && ShowFrontSide)
+            if (JapaneseToEnglishDirection && ShowFrontSide && SelectedIndexOfOtherForms >= 0)
             {
-                if (SelectedIndexOfOtherForms >= 0)
-                    outputText += !WriteInKana ? OtherForms.ElementAt(SelectedIndexOfOtherForms) : ReadingOutput;
+                outputTextStringBuilder.Append(!WriteInKana ? OtherForms.ElementAt(SelectedIndexOfOtherForms) : ReadingOutput);
             }
             else if (JapaneseToEnglishDirection && ShowBackSide)
             {
-                var meaningsString = string.Join("; ", Meanings.Where((_, i) => SelectedIndicesOfMeanings.Contains(i))); // TODO optimize
+                var meaningsString = string.Join("; ", Meanings.Select(g => g.SimilarMeanings)
+                                                               .SelectMany(g => g)
+                                                               .Where(m => m.IsEnabled)
+                                                               .Select(m => m.Value)); // TODO optimize
                 if (!WriteInKana)
                 {
-                    outputText += ReadingOutput;
+                    outputTextStringBuilder.Append(ReadingOutput);
                     if (!string.IsNullOrWhiteSpace(meaningsString))
-                        outputText += Environment.NewLine;
+                        outputTextStringBuilder.Append(Environment.NewLine);
                 }
-                outputText += meaningsString;
+                outputTextStringBuilder.Append(meaningsString);
                 if (!string.IsNullOrWhiteSpace(AdditionalComments))
                 {
-                    outputText += Environment.NewLine;
-                    outputText += AdditionalComments;
+                    outputTextStringBuilder.Append(Environment.NewLine);
+                    outputTextStringBuilder.Append(AdditionalComments);
                 }
             }
             else if (EnglishToJapaneseDirection && ShowFrontSide)
             {
-                var meaningsString = string.Join("; ", Meanings.Where((_, i) => SelectedIndicesOfMeanings.Contains(i))); // TODO optimize
-                outputText += meaningsString;
+                var meaningsString = string.Join("; ", Meanings.Select(g => g.SimilarMeanings)
+                                                               .SelectMany(g => g)
+                                                               .Where(m => m.IsEnabled)
+                                                               .Select(m => m.Value)); // TODO optimize
+                outputTextStringBuilder.Append(meaningsString);
                 if (!string.IsNullOrWhiteSpace(AdditionalComments))
                 {
-                    outputText += Environment.NewLine;
-                    outputText += AdditionalComments;
+                    outputTextStringBuilder.Append(Environment.NewLine);
+                    outputTextStringBuilder.Append(AdditionalComments);
                 }
             }
-            else //  (EnglishToJapaneseDirection && ShowBackSide)
+            else // (EnglishToJapaneseDirection && ShowBackSide)
             {
                 if (SelectedIndexOfOtherForms >= 0)
-                    outputText += !WriteInKana ? OtherForms.ElementAt(SelectedIndexOfOtherForms) : ReadingOutput;
+                    outputTextStringBuilder.Append(!WriteInKana ? OtherForms.ElementAt(SelectedIndexOfOtherForms) : ReadingOutput);
                 if (!WriteInKana)
                 {
-                    outputText += Environment.NewLine;
-                    outputText += ReadingOutput;
+                    outputTextStringBuilder.Append(Environment.NewLine);
+                    outputTextStringBuilder.Append(ReadingOutput);
                 }
             }
-            return outputText;
+            return outputTextStringBuilder.ToString();
         }
     }
-
-
+    
     partial void OnJapaneseToEnglishDirectionChanged(bool value) => UpdateOutputText();
 
     private bool EnglishToJapaneseDirection => !JapaneseToEnglishDirection;
@@ -148,16 +153,21 @@ public partial class JapaneseUserInputViewModel : JishoTangoAssistantViewModelBa
 
     partial void OnInputChanged(string value) => UpdateTextInputBackground();
 
-    partial void OnReadingOutputChanged(string value) => UpdateTextInputBackground();
+    partial void OnReadingOutputChanged(string value)
+    {
+        UpdateTextInputBackground();
+    }
 
     partial void OnAdditionalCommentsChanged(string value) 
     {
+        currentSelectionService.SetAdditionalComments(value);
         UpdateOutputText();
         UpdateTextInputBackground();
     }
 
     partial void OnWriteInKanaChanged(bool value) 
     {
+        currentSelectionService.SetWriteInKana(value);
         UpdateOutputText();
         UpdateTextInputBackground();
     }
@@ -166,7 +176,10 @@ public partial class JapaneseUserInputViewModel : JishoTangoAssistantViewModelBa
     {
         if (value >= 0)
         {
-            ChangeOtherForms();
+            currentSelectionService.SetSelectedWordsIndex(value);
+            currentSelectionService.UpdateOtherForms();
+            currentSelectionService.SetSelectedOtherFormsIndex(0);
+            UpdateAllNonCollectionProperties();
             UpdateTextInputBackground();
         }
     }
@@ -175,12 +188,11 @@ public partial class JapaneseUserInputViewModel : JishoTangoAssistantViewModelBa
     {
         if (value >= 0)
         {
+            currentSelectionService.SetSelectedOtherFormsIndex(value);
             UpdateOutputText();
             UpdateTextInputBackground();
         }
     }
-
-    partial void OnSelectedIndicesOfMeaningsChanged(ObservableCollection<int> value) => UpdateTextInputBackground();
     #endregion
 
     [RelayCommand]
@@ -189,7 +201,7 @@ public partial class JapaneseUserInputViewModel : JishoTangoAssistantViewModelBa
         if (CurrentSession.lastRetrievedResults == null)
             return;
 
-        var addedItem = CreateVocabularyItemFromCurrentUserInput();
+        var addedItem = currentSelectionService.CreateVocabularyItem();
 
         if (addedItem == null)
             return;
@@ -204,211 +216,64 @@ public partial class JapaneseUserInputViewModel : JishoTangoAssistantViewModelBa
         if (!CurrentSession.running)
         {
             CurrentSession.running = true;
-
             Input = RomajiKanaConverter.Convert(Input.Trim());
-
-            var allResults = await jishoWebService.GetResultJsonAsync(Input);
-            if (allResults == null || !allResults.Any())
-            {
-                ClearUserInputResults();
-                CurrentSession.running = false;
-
-                var mainWindow = ((IClassicDesktopStyleApplicationLifetime)Avalonia.Application.Current?.ApplicationLifetime!).MainWindow;
-
-                if (mainWindow != null && allResults == null) // Application could not retrieve information from Jisho
-                {
-                    await MessageBox.Show(mainWindow, "Error", "Information could not be retrieved!", MessageBoxButtons.Ok);
-                }
-                else if (mainWindow != null)
-                {
-                    await MessageBox.Show(mainWindow, "Information", "No results were found!", MessageBoxButtons.Ok);
-                }
-                return;
-            }
-
-            CurrentSession.lastRetrievedResults = allResults;
-
-            ClearUserInputResults();
-
-            var resultIndex = 0;
-            var entryIndex = 0;
-
-            if (WritingSystemChecker.ContainsKanji(Input))
-                GetIndicesOfInputInResult(allResults, ref resultIndex, ref entryIndex);
-
-            var result = allResults[resultIndex];
-            var japaneseEntry = result.Japanese[entryIndex];
-
-            foreach (var res in allResults)
-            {
-                var firstJapaneseResult = res.Japanese.First();
-                var word = !string.IsNullOrEmpty(firstJapaneseResult.Word) ? firstJapaneseResult.Word : firstJapaneseResult.Reading;
-                Words.Add(word);
-            }
-
-            if (Words.Any())
-            {
-                SelectedIndexOfWords = resultIndex;
-                if (OtherForms.Any())
-                    SelectedIndexOfOtherForms = entryIndex;
-            }
-
-            ReadingOutput = japaneseEntry.Reading;
-
-            WriteInKana = result.Senses.First().Tags.Contains(JishoTagUsuallyInKanaAlone)
-                || string.IsNullOrEmpty(japaneseEntry.Word);
-            ItemAdditionPossible = true;
+            await currentSelectionService.UpdateSelectionAsync(Input);
+            UpdateAllNonCollectionProperties();
             UpdateOutputText();
             CurrentSession.running = false;
         }
     }
 
 
-    private void GetIndicesOfInputInResult(IList<JishoDatum> result, ref int resultIndex, ref int entryIndex)
+    private void MeaningsUpdateCollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
     {
-        ArgumentNullException.ThrowIfNull(result);
-        ArgumentOutOfRangeException.ThrowIfNegative(resultIndex);
-        ArgumentOutOfRangeException.ThrowIfNegative(entryIndex);
-
-        for (var i = 0; i < result.Count; i++)
+        PropertyChangedEventHandler handler = (_, _) => UpdateOutputText();
+        if (e.NewItems != null)
         {
-            var res = result[i];
-            for (var j = 0; j < res.Japanese.Length; j++)
+            foreach (SimilarMeaningsGroup meaningsGroup in e.NewItems)
             {
-                var entry = res.Japanese[j].Word;
-                
-                if (Input != entry) continue;
-                
-                resultIndex = i;
-                entryIndex = j;
-
-                if (entryIndex == 0) // take result over entry of a prev result
-                    return;
-
-                break; // only take the first entry in the result
+                foreach (var meaning in meaningsGroup.SimilarMeanings)
+                {
+                    meaning.PropertyChanged += handler;
+                }
             }
         }
-
-        // default to index 0 if no result was found
-        resultIndex = Math.Max(resultIndex, 0);
-        entryIndex = Math.Max(entryIndex, 0);
-    }
-
-    private void ClearUserInputResults()
-    {
-        Meanings.Clear();
-        ReadingOutput = string.Empty;
-        Words.Clear();
-        OtherForms.Clear();
-
-        SelectedIndexOfWords = -1;
-        SelectedIndexOfOtherForms = -1;
-
-        AdditionalComments = string.Empty;
-        WriteInKana = false;
-
-        ClearCheckBoxesEvent?.Invoke();
-
-        ItemAdditionPossible = false;
-
-        UpdateOutputText();
-    }
-
-    private void ChangeOtherForms()
-    {
-        OtherForms.Clear();
-
-        var latestResult = CurrentSession.lastRetrievedResults;
-        if (latestResult == null)
-            return;
-        var selectedDatum = latestResult[SelectedIndexOfWords];
-        foreach (var japItem in selectedDatum.Japanese)
+        if (e.OldItems != null)
         {
-            var otherForm = !string.IsNullOrEmpty(japItem.Word) ? japItem.Word : japItem.Reading;
-            OtherForms.Add(otherForm);
+            foreach (SimilarMeaningsGroup meaningsGroup in e.OldItems)
+            {
+                foreach (var meaning in meaningsGroup.SimilarMeanings)
+                {
+                    meaning.PropertyChanged -= handler;
+                }
+            }
         }
-        if (OtherForms.Any())
-            SelectedIndexOfOtherForms = 0;
-        else
-            SelectedIndexOfOtherForms = -1;
-
-        ReadingOutput = selectedDatum.Japanese.First().Reading;
-
-        StoreMeanings(selectedDatum);
-
-        WriteInKana = selectedDatum.Senses.First().Tags.Contains(JishoTagUsuallyInKanaAlone)
-            || string.IsNullOrEmpty(selectedDatum.Japanese.First().Word);
     }
 
-    private void ChangeReadingOutput()
+    private void UpdateAllNonCollectionProperties()
     {
-        var latestResult = CurrentSession.lastRetrievedResults;
-
-        if (latestResult == null 
-            || 0 <= SelectedIndexOfWords && SelectedIndexOfWords < latestResult.Count 
-            || 0 <= SelectedIndexOfOtherForms && SelectedIndexOfOtherForms < latestResult[SelectedIndexOfWords].Japanese.Length)
-            return;
-
-        ReadingOutput = latestResult[SelectedIndexOfWords].Japanese[SelectedIndexOfOtherForms].Reading;
+        SelectedIndexOfWords = currentSelectionService.GetSelectedWordsIndex();
+        SelectedIndexOfOtherForms = currentSelectionService.GetSelectedOtherFormsIndex();
+        ReadingOutput = currentSelectionService.GetReadingOutput();
+        AdditionalComments = currentSelectionService.GetAdditionalComments();
+        WriteInKana = currentSelectionService.GetWriteInKana();
+        ItemAdditionPossible = currentSelectionService.GetItemAdditionPossible();
     }
 
-    private void StoreMeanings(JishoDatum datum)
-    {
-        Meanings.Clear();
-        var englishDefinitions = datum.Senses.SelectMany(sense => sense.EnglishDefinitions);
-        foreach (var meaning in englishDefinitions)
-        {
-            Meanings.Add(meaning);
-        }
-
-        UpdateCheckBoxesEvent?.Invoke(datum.Senses.Length, 
-            datum.Senses.Select(x => x.EnglishDefinitions.Length).ToList(), 
-            Meanings);
-    }
-
-    public void UpdateOutputText()
+    private void UpdateOutputText()
     {
         OnPropertyChanged(nameof(OutputText));
-    }
-
-    public void ClearSelectedIndicesOfMeanings()
-    {
-        SelectedIndicesOfMeanings.Clear();
-    }
-
-    public void ChangeSelectedIndicesOfMeanings(int i, bool isSelected)
-    {
-        if (isSelected)
-            SelectedIndicesOfMeanings.Add(i);
-        else
-            SelectedIndicesOfMeanings.Remove(i);
-        UpdateOutputText();
-    }
-
-    private VocabularyItem? CreateVocabularyItemFromCurrentUserInput()
-    {
-        if (SelectedIndexOfOtherForms < 0) // nothing has been searched (or no search results found)
-            return null;
-        var outputText = string.Empty;
-        var meaningsString = string.Join("; ", Meanings.Where((_, i) => SelectedIndicesOfMeanings.Contains(i))); // TODO optimize
-        outputText += meaningsString;
-        if (!string.IsNullOrWhiteSpace(AdditionalComments) 
-            && !string.IsNullOrWhiteSpace(meaningsString))
-            outputText += Environment.NewLine;
-        outputText += AdditionalComments;
-        var showReading = !WriteInKana;
-        var word = showReading ? OtherForms.ElementAt(SelectedIndexOfOtherForms) : ReadingOutput;
-        return new VocabularyItem(word, showReading, ReadingOutput, outputText);
     }
 
     private void UpdateTextInputBackground()
     {
         var color = InputTextColorNoDuplicate();
-        var itemFromCurrentUserInput = CreateVocabularyItemFromCurrentUserInput();
+        var itemFromCurrentUserInput = currentSelectionService.CreateVocabularyItem();
         if (itemFromCurrentUserInput != null && CurrentSession.VocabularyListService.ContainsWord(itemFromCurrentUserInput.Word))
         {
             color = CurrentSession.VocabularyListService.Contains(itemFromCurrentUserInput) ? InputTextColorSameMeaning() : InputTextColorDifferentMeaning();
         }
+        
         TextInputBackground = Color.Parse(color);
     }
     private string InputTextColorNoDuplicate()
