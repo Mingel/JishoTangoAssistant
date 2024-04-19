2df593930ca684ed8b5a5d33db8c6796ded30dfe
Minh Bang Vu <minhbangvu.sec@gmail.com>
2024-04-19 18:13:07 +0200

    Focus Input text box when new vocab item is added

--- JishoTangoAssistant/UI/Views/JapaneseUserInputView.axaml.cs
+++ JishoTangoAssistant/UI/Views/JapaneseUserInputView.axaml.cs
@@ -1,4 +1,6 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Interactivity;
 
 namespace JishoTangoAssistant.UI.Views;
@@ -12,9 +14,31 @@ public partial class JapaneseUserInputView : UserControl
     {
         InitializeComponent();
         Loaded += LoadedHandler;
+        KeyUp += KeyUpHandler;
     }
     
     private void LoadedHandler(object? sender, RoutedEventArgs? e)
+    {
+        InputTextBox.Focus();
+    }
+
+    private void AddToVocabularyListClickHandler(object? sender, RoutedEventArgs e)
+    {
+        FocusInputTextBox();
+    }
+    
+    private void KeyUpHandler(object? sender, KeyEventArgs e)
+    {
+        if (AddButton.IsEnabled &&
+            AddButton.HotKey != null &&
+            AddButton.HotKey.KeyModifiers == e.KeyModifiers &&
+            AddButton.HotKey.Key == e.Key)
+        {
+            FocusInputTextBox();
+        }
+    }
+
+    private void FocusInputTextBox()
     {
         InputTextBox.Focus();
     }
