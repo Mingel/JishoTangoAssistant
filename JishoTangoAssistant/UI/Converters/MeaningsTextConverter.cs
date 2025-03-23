1456c5de0caee54d0ecbeaae77939c303fd22fd8
Minh Bang Vu <minhbangvu.sec@gmail.com>
2025-03-23 13:19:33 +0100

    Rename Converter namespace

--- JishoTangoAssistant/UI/Converters/MeaningsTextConverter.cs
+++ JishoTangoAssistant/UI/Converters/MeaningsTextConverter.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using Avalonia.Data.Converters;
-using Avalonia.Input;
 
-namespace JishoTangoAssistant.UI.Converter;
+namespace JishoTangoAssistant.UI.Converters;
 
 public class MeaningsTextConverter : IValueConverter
 {
